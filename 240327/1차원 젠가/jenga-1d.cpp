90068651e299695dd8421550821f69df1c14675f
박찬준 <100582352+Lunawood@users.noreply.github.com>
2024-03-27 21:32:10 +0900

    Title: 1차원 젠가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240327/1차원 젠가/jenga-1d.cpp
+++ 240327/1차원 젠가/jenga-1d.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int n;
+int n, N;
 int arr[200];
 int idx1, idx2;
 
 int main() {
     cin >> n;
-    for(int i = 1; i <= n; i++){
+    N = n;
+    for (int i = 1; i <= n; i++) {
         cin >> arr[i];
     }
-    for(int i = 0; i < 2; i++){
+    for (int i = 0; i < 2; i++) {
         cin >> idx1 >> idx2;
-        n -= (idx2 - idx1 + 1);
-        for(int i = 0; i < idx2 - idx1 + 1; i++){
+        N -= (idx2 - idx1 + 1);
+        for (int i = 0; i < n - idx2 + 1; i++) {
             arr[i + idx1] = arr[i + idx2 + 1];
         }
     }
-    cout << n << "\n";
-    for(int i = 1; i <= n; i++){
+    cout << N << "\n";
+    for (int i = 1; i <= N; i++) {
         cout << arr[i] << "\n";
     }
     return 0;
