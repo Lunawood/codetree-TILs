6e713aec70639380f88f28db89e3fd8687b982e6
박찬준 <100582352+Lunawood@users.noreply.github.com>
2024-07-09 23:17:56 +0900

    Title: 최고의 33위치, Time: 125ms, Memory: 72MB, Status: Passed - CodeTree

---
+++ 240709/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,21 @@
+# 입력값
+n = int(input())
+li = list()
+for i in range(n):
+    li.append(list(map(int, input().split())))
+
+move = n - 2
+ans = 0
+# 3 x 3이 움직일 수 있는 크기
+for i in range(move):
+    for j in range(move):
+        cnt = 0
+        # 3 x 3으로 check
+        for k in range(3):
+            for l in range(3):
+                if li[k + i][l + j] == 1:
+                    cnt += 1
+        if ans < cnt:
+            ans = cnt
+
+print(ans)
