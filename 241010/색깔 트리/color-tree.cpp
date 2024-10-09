f2d3c12c76455e75c660f8fcdabc4c53e852bdb1
박찬준 <100582352+Lunawood@users.noreply.github.com>
2024-10-10 06:36:54 +0900

    Title: 색깔 트리, Time: 309ms, Memory: 2MB, Status: Passed - CodeTree

--- 241010/색깔 트리/color-tree.cpp
+++ 241010/색깔 트리/color-tree.cpp
@@ -8,10 +8,11 @@ struct Node{
     int pid;
     int color;
     int depth;
+    int priority;
     vector<int> children;
 };
 
-vector<int> all;
+vector<int> roots;
 unordered_map<int, Node> map;
 
 bool addComfirm(int pid) {
@@ -33,55 +34,94 @@ void add(int mid, Node node) {
         if (node.pid != -1) {
             map[node.pid].children.emplace_back(mid);
         }
-        all.emplace_back(mid);
+        else {
+            roots.emplace_back(mid);
+        }
     }
 }
 
-void collectColor(int mid, set<int> &distinctColor) {
-    distinctColor.emplace(map[mid].color);
 
-    for (int i = 0; i < map[mid].children.size(); i++) {
-        collectColor(map[mid].children[i], distinctColor);
+
+pair<int, int[6]> collect(int mid, int lastColor, int lastPriority) {
+    int cur_priority = map[mid].priority;
+
+    if (cur_priority >= lastPriority) {
+        lastPriority = cur_priority;
+        lastColor = map[mid].color;
     }
+
+    pair<int, int[6]> result;
+    result.first = 0;
+    for (int i = 1; i < 6; i++) {
+        result.second[i] = 0;
+    }
+    result.second[lastColor] = 1;
+    for (auto p : map[mid].children) {
+        pair<int, int[6]> subResult = collect(p, lastColor, lastPriority);
+        for (int i = 1; i < 6; i++) {
+            result.second[i] += subResult.second[i];
+        }
+        result.first += subResult.first;
+    }
+    int value = 0;
+    for (int i = 1; i < 6; i++) {
+        if (result.second[i] > 0)
+            value++;
+    }
+    result.first += value * value;
+    
+    return result;
 }
 
 int printScore() {
     int result = 0;
 
-    for (auto p : all) {
-        set<int> distinctColor;
-        collectColor(p, distinctColor);
-        int eachValue = distinctColor.size();
-        result += (eachValue * eachValue);
+    for (auto root : roots) {
+        int eachValue = collect(root, map[root].color, map[root].priority).first;
+        result += eachValue;
     }
     
     return result;
 }
 
 int searchColor(int mid) {
-    return map[mid].color;
+    int max = 0;
+    int color = map[mid].color;
+    int cur_mid = mid;
+
+    while (cur_mid != -1) {
+        if (max < map[cur_mid].priority) {
+            max = map[cur_mid].priority;
+            color = map[cur_mid].color;
+        }
+
+        cur_mid = map[cur_mid].pid;
+    }
+
+    return color;
 }
 
-void updateColor(int mid, int color) {
+void updateColor(int mid, int color, int priority) {
     map[mid].color = color;
-    for (auto child : map[mid].children) {
-        updateColor(child, color);
-    }
+    map[mid].priority = priority;
 }
 
 int main() {
     int Q, c, mid, pid, color, depth;
+    int priority = 1;
     cin >> Q;
     for (int i = 0; i < Q; i++) {
         cin >> c;
         if (c == 100) { // ADD
             cin >> mid >> pid >> color >> depth;
-            Node node({ pid, color, depth });
+            Node node({ pid, color, depth, priority});
             add(mid, node);
+            priority++;
         }
         else if (c == 200) { // Change Color
             cin >> mid >> color;
-            updateColor(mid, color);
+            updateColor(mid, color, priority);
+            priority++;
         }
         else if (c == 300) { // Search Color
             cin >> mid;
